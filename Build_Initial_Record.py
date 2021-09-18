eede3ef37c8e3690fc79e9f0297cf488fb76c59d
jimmy-zhangjinshui <69950916+jimmy-zhangjinshui@users.noreply.github.com>
2021-09-18 12:54:49 -0400

    finished

--- Build_Initial_Record.py
+++ Build_Initial_Record.py
@@ -1,5 +1,5 @@
 import pickle
-from datetime import date
+import time
 
 def save_obj(obj, name):
     with open(name + '.pkl', 'wb') as f:
@@ -20,9 +20,10 @@ time_record['Machine Learning']['initial'] = 200.0
 time_record['Power Electronics']['initial'] = 3000.0
 time_record['Neuroscience']['initial'] = 50.0
 
-passcode = input('You are resetting all the time records before, to go on please input the passcode __________')
-if passcode != '123459':
+passcode = input('You are resetting all the time records before, to go on please input the passcode __________\n')
+if passcode == '123459':
+    save_obj(time_record, 'record')
+    print('Reset successful.')
+else:
     print('passcode worng, now quitting.')
-    quit()
-save_obj(time_record, 'record')
-print('Reset successful.')
+time.sleep(3)
--- update_daily.py
+++ update_daily.py
@@ -2,14 +2,15 @@ import pickle
 from datetime import date
 import tkinter as tk
 import tkinter.ttk as ttk
+import time
 
 def save_obj(obj, name):
     with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
 def load_obj(name ):
     with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
+
 time_record = load_obj('record')
 
 master = tk.Tk()
@@ -22,8 +23,6 @@ for no, key in enumerate(time_record_keys):
     globals()['progress_bar%s'%no] = ttk.Progressbar(master, orient="horizontal", mode="determinate", maximum=100, value=sum(time_record[key].values()) / 100.0)
     globals()['progress_bar%s'%no].grid(row=no, column=2)
     
-
-
 def save_data_button(time_record):
     time_record_keys = time_record.keys()
     for no, key in enumerate(time_record_keys):
@@ -35,10 +34,11 @@ def update_progress(time_record):
 
 tk.Button(master, text='Save', command= lambda: save_data_button(time_record)).grid(row=len(time_record)+1, column=1, sticky=tk.W, pady=4)
 tk.Button(master, text='Update', command= lambda: update_progress(time_record)).grid(row=len(time_record)+1, column=2, sticky=tk.W, pady=4)
-
 tk.Button(master, text='Quit', command=master.quit).grid(row=len(time_record)+1, column=0, sticky=tk.W, pady=4)
+
 
 tk.mainloop()
 
+time.sleep(3)
 save_obj(time_record, 'record')
 
