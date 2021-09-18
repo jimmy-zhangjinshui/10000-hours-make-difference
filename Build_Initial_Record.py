2c4c6fab17bd1c2814020c9b0ceaaaecad557fbb
jimmy-zhangjinshui <69950916+jimmy-zhangjinshui@users.noreply.github.com>
2021-09-18 11:52:26 -0400

    10000

---
+++ Build_Initial_Record.py
@@ -0,0 +1,28 @@
+import pickle
+from datetime import date
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+My_Things = ['Python', 'Body Building', 'Machine Learning', 'Power Electronics', 'Neuroscience']
+time_record = {}
+for thing in My_Things:
+    time_record[thing] = {}
+
+time_record['Python']['initial'] = 400.0
+time_record['Body Building']['initial'] = 500.0
+time_record['Machine Learning']['initial'] = 200.0
+time_record['Power Electronics']['initial'] = 3000.0
+time_record['Neuroscience']['initial'] = 50.0
+
+passcode = input('You are resetting all the time records before, to go on please input the passcode __________')
+if passcode != '123459':
+    print('passcode worng, now quitting.')
+    quit()
+save_obj(time_record, 'record')
+print('Reset successful.')
---
+++ update_daily.py
@@ -0,0 +1,44 @@
+import pickle
+from datetime import date
+import tkinter as tk
+import tkinter.ttk as ttk
+
+def save_obj(obj, name):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+time_record = load_obj('record')
+
+master = tk.Tk()
+time_record_keys = time_record.keys()
+for no, key in enumerate(time_record_keys):
+    tk.Label(master, text = key).grid(row = no)
+    globals()['entry%s'%no] = tk.Entry(master)
+    globals()['entry%s'%no].insert(-1, 0.0)
+    globals()['entry%s'%no].grid(row=no, column=1)
+    globals()['progress_bar%s'%no] = ttk.Progressbar(master, orient="horizontal", mode="determinate", maximum=100, value=sum(time_record[key].values()) / 100.0)
+    globals()['progress_bar%s'%no].grid(row=no, column=2)
+    
+
+
+def save_data_button(time_record):
+    time_record_keys = time_record.keys()
+    for no, key in enumerate(time_record_keys):
+        time_record[key][date.today()] = float(globals()['entry%s'%no].get())
+def update_progress(time_record):
+    time_record_keys = time_record.keys()
+    for no, key in enumerate(time_record_keys):
+        globals()['progress_bar%s'%no].value=sum(time_record[key].values()) / 100.0 + 10
+
+tk.Button(master, text='Save', command= lambda: save_data_button(time_record)).grid(row=len(time_record)+1, column=1, sticky=tk.W, pady=4)
+tk.Button(master, text='Update', command= lambda: update_progress(time_record)).grid(row=len(time_record)+1, column=2, sticky=tk.W, pady=4)
+
+tk.Button(master, text='Quit', command=master.quit).grid(row=len(time_record)+1, column=0, sticky=tk.W, pady=4)
+
+tk.mainloop()
+
+save_obj(time_record, 'record')
+
